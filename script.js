25d8293308b0b367897812ee92f6a44e96185ee8
Lukee112 <learl7999@gmail.com>
2024-02-05 20:57:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Plan to use * - + for my 3 calculations 
+// they are gonna be in order of the lock combination for the vault.
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+// vault codes in order
+const num1 = 5 * 2
+const num2 = 60-20
+const num3 = 36 + 3
+// popup for vault codes
+alert (num1)
+alert (num2)
+alert(num3)
